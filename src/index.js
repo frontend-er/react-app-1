095d446602b5ced63ede5e8d8904023d5e4377c7
Dimas <dbjenov@gmail.com>
2021-05-19 12:11:44 +0300

    this and kontext-vizova))

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {update} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {addPost, updatePost, addMessage, updateMessage} from "./redux/state";
 
 let renderComponents = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updatePost={updatePost} addMessage={addMessage} updateMessage={updateMessage} />
+      <App state={state} addPost={store.addPost.bind(store)} updatePost={store.updatePost.bind(store)} addMessage={store.addMessage.bind(store)} updateMessage={store.updateMessage.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
   );
@@ -17,8 +16,8 @@ let renderComponents = (state) => {
 }
 
 
-renderComponents(state);
-update(renderComponents);
+renderComponents(store.getState());
+store.update(renderComponents);
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,137 +1,152 @@
-let renderComponents = () => {
-   console.log("render");
-};
+let store = {
+   _state: {
+      profilePage: {
+         posts: [{
+               id: 1,
+               name: "newPost",
+               likesOnPost: 12
+            },
+            {
+               id: 2,
+               name: "newPost 2 ",
+               likesOnPost: 11
+            },
 
-let state = {
+         ],
 
-   profilePage: {
-      posts: [{
-            id: 1,
-            name: "newPost",
-            likesOnPost: 12
-         },
-         {
-            id: 2,
-            name: "newPost 2 ",
-            likesOnPost: 11
-         },
+         newPostValue: 'facebook'
+      },
 
-      ],
+      messagesPage: {
 
-      newPostValue: 'facebook'
+         dialogs: [{
+               id: '1',
+               name: "Dimas",
+               img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
+            },
+            {
+               id: "2",
+               name: "Oleg",
+               img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
+            },
+            {
+               id: "3",
+               name: "Ann",
+               img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
+            },
+            {
+               id: "4",
+               name: "Sveta",
+               img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
+            },
+
+         ],
+
+         messages: [{
+               id: '1',
+               name: "Hi"
+            },
+            {
+               id: "2",
+               name: "Hellow oleg"
+            },
+            {
+               id: "3",
+               name: "how are u?"
+            },
+            {
+               id: "4",
+               name: "Sveta"
+            },
+
+         ],
+
+         defaultMessage: 'Write your message:'
+      },
+
+
+      sideBar: {
+         friends: [{
+               id: "1",
+               name: "Andrey",
+               img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
+            },
+            {
+               id: "2",
+               name: "Shasa",
+               img: 'https://cdn2.vectorstock.com/i/1000x1000/13/76/icon-user-avatar-for-web-site-or-mobile-vector-4031376.jpg'
+
+            },
+            {
+               id: "3",
+               name: "Andrey",
+               img: 'https://cdn2.vectorstock.com/i/1000x1000/13/76/icon-user-avatar-for-web-site-or-mobile-vector-4031376.jpg'
+
+            }
+         ]
+
+      }
+
+
+
    },
 
-   messagesPage: {
+   getState() {
+      return this._state;
+   },
 
-      dialogs: [{
-            id: '1',
-            name: "Dimas",
-            img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
-         },
-         {
-            id: "2",
-            name: "Oleg",
-            img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
-         },
-         {
-            id: "3",
-            name: "Ann",
-            img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
-         },
-         {
-            id: "4",
-            name: "Sveta",
-            img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
-         },
+   renderComponents() {
+      console.log("render");
+   },
 
-      ],
+   addMessage() {
+      let message = {
+         id: '5',
+         name: this._state.messagesPage.defaultMessage,
+      }
 
-      messages: [{
-            id: '1',
-            name: "Hi"
-         },
-         {
-            id: "2",
-            name: "Hellow oleg"
-         },
-         {
-            id: "3",
-            name: "how are u?"
-         },
-         {
-            id: "4",
-            name: "Sveta"
-         },
-
-      ],
-
-      defaultMessage: 'Write your message:'
+      this._state.messagesPage.messages.push(message);
+      this._state.messagesPage.defaultMessage = '';
+      this.renderComponents(this._state);
+   },
+   updateMessage(newValue) {
+      this._state.messagesPage.defaultMessage = newValue;
+      this.renderComponents(this._state);
    },
 
 
-   sideBar: {
-      friends: [{
-            id: "1",
-            name: "Andrey",
-            img: 'https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg'
-         },
-         {
-            id: "2",
-            name: "Shasa",
-            img: 'https://cdn2.vectorstock.com/i/1000x1000/13/76/icon-user-avatar-for-web-site-or-mobile-vector-4031376.jpg'
 
-         },
-         {
-            id: "3",
-            name: "Andrey",
-            img: 'https://cdn2.vectorstock.com/i/1000x1000/13/76/icon-user-avatar-for-web-site-or-mobile-vector-4031376.jpg'
+   update(observer) {
+      this.renderComponents = observer;
+   },
 
-         }
-      ]
+   addPost() {
+      let post = {
+         id: '5',
+         name: this._state.profilePage.newPostValue,
+         likesOnPost: 11
+      }
 
+      this._state.profilePage.posts.push(post);
+      this._state.profilePage.newPostValue = '';
+
+      this.renderComponents(this._state);
+   },
+
+   updatePost(newValue) {
+      this._state.profilePage.newPostValue = newValue;
+      this.renderComponents(this._state);
    }
-
-
-
-}
-window.state = state;
-
-export let addMessage = () => {
-   let message = {
-      id: '5',
-      name: state.messagesPage.defaultMessage,
-   }
-
-   state.messagesPage.messages.push(message);
-   state.messagesPage.defaultMessage = '';
-   renderComponents(state);
 }
 
-export let updateMessage = (newValue) => {
-   state.messagesPage.defaultMessage = newValue;
-   renderComponents(state);
-}
 
-export const update = (observer) => {
-   renderComponents = observer;
-}
 
-export let addPost = () => {
-   let post = {
-      id: '5',
-      name: state.profilePage.newPostValue,
-      likesOnPost: 11
-   }
 
-   state.profilePage.posts.push(post);
-   state.profilePage.newPostValue = '';
 
-   renderComponents(state);
-}
 
-export let updatePost = (newValue) => {
-   state.profilePage.newPostValue = newValue;
-   renderComponents(state);
-}
 
-export default state;
+
+
+
+window.store = store;
+export default store;
