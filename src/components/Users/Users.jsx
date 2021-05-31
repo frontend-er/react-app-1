a83a8c748477770cda1d1324a796f674365153ca
Dimas <dbjenov@gmail.com>
2021-05-31 10:33:57 +0300

    axios API i and server work

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import style from './Users.module.css'
+import style from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from "../../assets/img/profilePhoto.png";
 
 function Users(props)  {
+
+   if (props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+         debugger;
+         props.setUsers(response.data.items);
+      })
+   }
+
    return (
       <div>
          {
             props.users.map(u => <div key ={u.id}>
                <span>
                   <div className={style.avatar}>
-                     <img src={u.avatarPhoto} alt="" />
+                     <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="" />
                   </div>
                   <div>
                      {u.followed ? <button onClick={() => {props.unfollow(u.id)}}>Follow</button> : <button  onClick={() => {props.follow(u.id)}}>Unfolow</button>}
@@ -16,12 +26,12 @@ function Users(props)  {
                </span>
                <span>
                   <span>
-                     <div>{u.fullName}</div>
+                     <div>{u.name}</div>
                      <div>{u.satus}</div>
                   </span>
                   <span>
-                     <div>{u.location.city}</div>,
-                     <div>{u.location.country}</div>
+                     <div>{"u.location.city"}</div>,
+                     <div>{"u.location.country"}</div>
                   </span>
                </span>
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,37 +4,7 @@ const SET_USERS = 'SET_USERS';
 
 
 let initialState = {
-   users: [{
-      id: 1,
-      followed: false,
-      avatarPhoto: 'https://content.fortune.com/wp-content/uploads/2020/09/CNV.10.20.FORTUNE_BILL_AND_MELINDA_GATES_030-vertical.jpg',
-      fullName: 'Dmity O',
-      status: 'I am a programmer',
-      location: {
-         city: 'Chisinau',
-         country: 'Moldova'
-      }
-   }, {
-      id: 2,
-      followed: false,
-      avatarPhoto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Elon_Musk_-_The_Summit_2013.jpg/200px-Elon_Musk_-_The_Summit_2013.jpg',
-      fullName: 'Ann U',
-      status: 'I am a teacher',
-      location: {
-         city: 'Chisinau',
-         country: 'Moldova'
-      }
-   }, {
-      id: 3,
-      followed: true,
-      avatarPhoto: "https://variety.com/wp-content/uploads/2019/12/jeff_bezos2019_rev-1.png",
-      fullName: 'Mark Zukerberg',
-      status: 'I am a boss',
-      location: {
-         city: 'Moscow',
-         country: 'Russia'
-      }
-   }]
+   users: []
 };
 
 const usersReducer = (state = initialState, action) => {
