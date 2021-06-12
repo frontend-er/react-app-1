266b3d1c9809a31504ee1fb9ebf519f227a13565
Dimas <dbjenov@gmail.com>
2021-06-12 14:47:21 +0300

    Action creator

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,18 +4,17 @@ import { follow,unfollow,setUsers, setCurrentPage, setUsersTotalCount, toggleIsF
 import * as axios from 'axios';
 import Users from "./Users";
 import Loader from "./../common/Loader/Loader";
+import { getUsers } from "./../api/api";
 
 
 class UsersContainer extends React.Component {
    componentDidMount () {
       this.props.toggleIsFetching(true);
-         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, 
-         {
-            withCredentials: true
-         }).then(response => {
+        getUsers(this.props.currentPage, this.props.pageSize)
+        .then(response => {
                this.props.toggleIsFetching(false);
-               this.props.setUsers(response.data.items);
-               this.props.setUsersTotalCount(response.data.totalCount);
+               this.props.setUsers(response.items);
+               this.props.setUsersTotalCount(response.totalCount);
 
          })
    }
@@ -24,12 +23,10 @@ class UsersContainer extends React.Component {
       return (
          this.props.toggleIsFetching(true),
          this.props.setCurrentPage(pageNumber),
-         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,  
-         {
-            withCredentials: true
-         }).then(response => {
+         getUsers(pageNumber, this.props.pageSize)
+         .then(response => {
                this.props.toggleIsFetching(false);
-               this.props.setUsers(response.data.items);
+               this.props.setUsers(response.items);
          })
       )
    }
---
+++ src/components/api/api.js
@@ -0,0 +1,16 @@
+import * as axios from 'axios';
+
+
+const instance = axios.create({
+   baseURL: "https://social-network.samuraijs.com/api/1.0/",
+   withCredentials: true,
+   headers: {
+      "API-KEY": "4e0e2285-df51-4910-837e-25942ded4b41"
+   }
+})
+
+
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+   return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => response.data)
+}
