567ff3fcba85500d722ed03dda469556da20f0e1
Dimas <dbjenov@gmail.com>
2021-06-16 13:53:04 +0300

    satus on Click

--- src/components/Profile/UserProfile/UserInfo/Status.jsx
+++ src/components/Profile/UserProfile/UserInfo/Status.jsx
@@ -3,14 +3,30 @@ import  React from 'react';
 
 
 class Status extends React.Component{
+   state = {
+      editMode: false
+   }
+
+   activateeditMode () {
+      this.setState({
+         editMode: true
+      })
+   }
+
+    deactivateeditMode () {
+      this.setState({
+         editMode: false
+      })
+   }
+
 
    render() {
       return (
          <div>
-            {this.props.status}
-            <input calue={this.props.status} />
+            {!this.state.editMode && <div onDoubleClick={this.activateeditMode.bind(this)}>{this.props.status}</div> }
+            {this.state.editMode && <input autoFocus={true} onBlur={this.deactivateeditMode.bind(this)} value={this.props.status} />}
          </div>
-   )
+      )
    }
    
 }
